e666ec4545f97c0859fb0ce048a57b50c0332899
Ryu0012 <93696435+Ryu0012@users.noreply.github.com>
2022-09-08 13:24:30 +0900

    BeautifulSoup&selenium

--- main.py
+++ main.py
@@ -1,36 +1,9 @@
-from requests import get
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
-base_url = "https://weworkremotely.com/remote-jobs/search?term="
-search_term = "python"
+print("start!")
+driver = webdriver.Chrome("C:/Users/82109/Desktop/chromedriver_win32/chromedriver.exe")
 
-response = get(f"{base_url}{search_term}")
-if response.status_code != 200:
-  print("Can't requests website!")
-else:
-  results = []
-  soup = BeautifulSoup(response.text, "html.parser")
-  jobs = soup.find_all('section', class_="jobs")
-
-  for job_section in jobs:
-    job_posts = job_section.find_all('li')
-
-    job_posts.pop(-1)
-    for post in job_posts:
-      anchors = post.find_all('a')
-      anchor = anchors[1]
-      link = anchor['href']
-
-      company, kind, region = anchor.find_all('span', class_="company")
-      title = anchor.find('span', class_="title")
-      job_data = {
-        'link':f"https://weworkremotely.com/{link}",
-        'company': company.string,
-        'kind': kind.string,
-        'region': region.string
-      }
-      results.append(job_data)
-
-for result in results:
-  print(result)
-  print("\n")
+driver.implicitly_wait(3)
+driver.get("https://kr.indeed.com/jobs?q=python&l=&vjk=d364360d1e0368e3")
