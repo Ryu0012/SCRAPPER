bac8c115433bc6eaa7d630f4148bd43e0a3a605d
Ryu0012 <93696435+Ryu0012@users.noreply.github.com>
2022-10-12 16:22:49 +0900

    flask로 서버 구축

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from bs4 import BeautifulSoup
 from wwr_ import wwr_job_scrapper
 from save_to_file import save_to_file
+from flask import Flask, render_template
+
 
 keyword = input("keyword: ")
 jobs = wwr_job_scrapper(keyword)
 
 # save_to_file(keyword, jobs)
+app = Flask("job Scrapper")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", name="lvu")
+
+
+app.run("0.0.0.0")
